e0c8795dfaa2393fddecde9f5b06b43cea3c6673
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2023-07-03 14:44:52 +0700

    update database config

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
@@ -33,35 +34,39 @@ func init() {
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
 
+	var err error
+
 	if dbConnection == "mysql" {
 		// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True"
 		dsn := fmt.Sprint(dbUsername, ":", dbPassword, "@tcp(", dbHost, ":", dbPort, ")/", dbName, "?charset=utf8mb4&parseTime=True")
-		db, err := gorm.Open(mysql.New(mysql.Config{
+		Db, err := gorm.Open(mysql.New(mysql.Config{
 			DSN: dsn,
 		}), &gorm.Config{
 			SkipDefaultTransaction: true,
 			PrepareStmt:            true,
 		})
-
-		if err != nil {
-			log.Panic(err)
-		}
-
-		Db = db
 	} else if dbConnection == "pgsql" {
 		// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
 		dsn := fmt.Sprint("host=", dbHost, " user=", dbUsername, " password=", dbPassword, " dbname=", dbName, " port=", dbPort, " sslmode=disable")
-		db, err := gorm.Open(postgres.New(postgres.Config{
+		Db, err := gorm.Open(postgres.New(postgres.Config{
 			DSN: dsn,
 		}), &gorm.Config{
 			SkipDefaultTransaction: true,
 			PrepareStmt:            true,
 		})
+	}
 
-		if err != nil {
-			log.Panic(err)
-		}
+	if err != nil {
+		log.Panic(err)
+	}
 
-		Db = db
+	sqlDB, err := Db.DB()
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(1000)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	if err != nil {
+		log.Panic(err)
 	}
 }
